50f390290c3ab3bd20f08f378d4ef9d391d83297
krishna pandey <krishna7376285419@gmail.com>
2026-06-28 11:12:17 +0530

    Backend lec-1,2,3,4

---
+++ Lecture 2/first.js
@@ -0,0 +1,7 @@
+console.log("krishna apndey")
+
+const fs = require('fs');
+fs.writeFile("output.txt","Writing File",(err)=>{
+  if(err ) console.log("Error occured");
+  else console.log("File written successfully")
+})
